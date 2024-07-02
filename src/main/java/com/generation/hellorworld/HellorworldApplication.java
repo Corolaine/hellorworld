f8226fdbbc7095e713902663b0f06dc04625017c
Corolaine <contacaroline.coutinho@gmail.com>
2024-07-01 23:50:49 -0300

    Initial commit

---
+++ src/main/java/com/generation/hellorworld/HellorworldApplication.java
@@ -0,0 +1,14 @@
+package com.generation.hellorworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellorworldApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HellorworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/hellorworld/HellorworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.hellorworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellorworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
